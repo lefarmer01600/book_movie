69827f7bf0d8edee0b203f6b55214fced1b32818
Taazwind <tazwindtazwind@gmail.com>
2025-04-03 15:11:08 +0200

    Connect to MongoDB and export the mongoose connection

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 3000;
+const MONGO_URI = 'mongodb://localhost:27017/book_movies';
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Failed to connect to MongoDB', err));
+
+// Export the mongoose connection to use in other files
+module.exports = mongoose.connection;
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
