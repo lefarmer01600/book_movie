7866f7ece3954b915c43c683c23ad93a3818a838
Kevynflrs <k.floresdacosta@ecoles-epsi.net>
2025-04-17 14:15:22 +0200

    ajout d'un regex pour l'email et modif front

--- public/js/inscription.js
+++ public/js/inscription.js
@@ -3,9 +3,24 @@ document.getElementById('submitRegister').addEventListener('click', async () => 
     const form = document.getElementById('registerForm');
     const formData = new FormData(form);
 
+    const email = formData.get('email');
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    // Vérification si l'email est vide
+    if (!email) {
+        alert('Veuillez saisir une adresse email.');
+        return; // Bloque l'envoi
+    }
+
+    // Vérification si l'email est valide
+    if (!emailRegex.test(email)) {
+        alert('Veuillez entrer une adresse email valide.');
+        return; // Bloque l'envoi
+    }
+
     const data = {
         name: formData.get('name'),
-        email: formData.get('email'),
+        email: email,
         passwordHash: CryptoJS.SHA256(formData.get('password')).toString(CryptoJS.enc.Base64),
     };
 
